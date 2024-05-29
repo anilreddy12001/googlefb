55fff4f188e1a73aeaaf569eacea160795e3c906
anilkumarrk <anilkumar.rk@axiscades.in>
2024-05-29 10:40:37 +0530

    added build files

---
+++ public/config.js
@@ -0,0 +1,24 @@
+env='development';
+dev_ip="http://192.168.41.54";
+qa_ip="http://192.168.41.103";
+prod_ip="http://192.168.41.143";
+config = {
+    development:{
+      latestUpdatedDate:"6-Feb-2024",
+		HBLStartAPI:  "http://localhost:8099/holding-inventory-control/api/v1/material/1/start",
+
+HBLUploadAPI: "http://localhost:8099/holding-inventory-control/api/v1/material/upload",
+
+HBLComponentsAPI: "http://localhost:8099/holding-inventory-control/api/v1/material/1/components",
+
+HBLProcessAPI: "http://localhost:8099/holding-inventory-control/api/v1/material/1/process",
+apiKey: "AIzaSyDk-GxAWYP4MJyWnz3_hb5C-_jmGOBAOF4",
+  authDomain: "anilproject12001.firebaseapp.com",
+  databaseURL: "https://anilproject12001.firebaseio.com",
+  projectId: "anilproject12001",
+  storageBucket: "anilproject12001.appspot.com",
+  messagingSenderId: "238384958064",
+  appId: "1:238384958064:web:68ab59a96c78675d6ac2b5",
+  measurementId: "G-K2WPZWH6X7"
+}
+}
